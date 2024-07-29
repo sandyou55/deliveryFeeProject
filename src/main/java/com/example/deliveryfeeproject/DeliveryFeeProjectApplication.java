e24d9b55ce9a2dbd064c4955595a3b9f85a116e1
kim-won-woo <ow0531@naver.com>
2024-07-29 21:10:59 +0900

    new

---
+++ src/main/java/com/example/deliveryfeeproject/DeliveryFeeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.deliveryfeeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryFeeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryFeeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deliveryfeeproject/DeliveryFeeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deliveryfeeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryFeeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
